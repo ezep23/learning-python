079320abf4ed8541841a0838aa291559448fb0fa
Ezequiel Pistone <ezequielpistoneee@gmail.com>
2024-04-08 23:06:38 -0300

    operaciones aritmeticas y comentarios

--- index.py
+++ index.py
@@ -3,3 +3,41 @@ print("Hola, Mundo")
 year = 2024
 
 print ("Estamos en el año: ", year)
+
+""" Algoritmo basico para actividad:
+
+    1. Ir hacia la heladera 
+    2. Abrir la puerta de la heladera
+    3. estirar el brazo y agarrar la jarra de jugo
+    4. sacar el brazo de la heladera y cerrar su puerta
+    5. colocar la jarra en la mesa
+    6. buscar un vaso de vidrio que este limpio sobre la mesada
+    7. agarrar el vaso, ponelo a 5 centimetros de la jarra.
+    8. agarrar la jarra, posicionarlo a 30 centimetros de altura sobre el vaso e inclinarlo a 45 grados
+    9. levantar de vuelta a 90 grados cuando este por la mitad de capacidad nuestro vaso.
+
+""" 
+
+rol = amigo
+nombre =  gervacio 
+
+print("Buen día, me gustaria invitarte mi buen, ", rol, " a mi cumpleaños el día 8 de abril, te espero ", nombre)
+ 
+hw = ¡Hola, Mundo!
+print(hw)
+
+numero = 5 
+snumero = 10
+print("Mi numero es ", numero)
+
+resultado_suma = snumero + numero
+print(resultado)
+
+resultado_resta = snumero - numero
+print(resultado_resta)
+
+resultado_multiplicacion = snumero*numero
+print(resultado_multiplicacion)
+
+resultado_division = snumero/numero
+print(resultado_division)
